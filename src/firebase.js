91b55ac910a5cf65e9a7f7165bf018a53122d27d
andrewcook <andrewjcook93@gmail.com>
2024-02-26 07:13:50 -0600

    Feature: basic edit profile form

--- src/firebase.js
+++ src/firebase.js
@@ -7,7 +7,7 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCSEzu_mx5MYVDQjpS4iDhNoE1ecLrLrzw",
+  apiKey: "",
   authDomain: "movies-56943.firebaseapp.com",
   projectId: "movies-56943",
   storageBucket: "movies-56943.appspot.com",
